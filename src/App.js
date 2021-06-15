c0d40ba741b12dd282590e257a8f4bc56d1a41d2
jagrati-c <panchaljagrati1997@gmail.com>
2021-06-15 14:57:08 +0530

    style updated in service card

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Routes from "./Page/Routes.jsx";
 function App() {
   return (
     <div>
-      {/* <Routes /> */}
+      <Routes />
       {/* <MensHairCutAndGrooming /> */}
-      <AcServiceAndRepairModal />
+      {/* <AcServiceAndRepairModal /> */}
     </div>
   );
 }
